1dee3c3c61f17b4f2e2328de9d061b3f0c453b72
DESKTOP-02RE0NO\Kyungtaek <kyungtaek0204@gmail.com>
2024-03-02 17:47:25 +0900

    [Add] 공 튀기기 구현, 더블버퍼링 구현, 메모리 DC 공부 등

---
+++ second_practice/Project1/Bitmap_Study/Main.cpp
@@ -0,0 +1,120 @@
+﻿#include <Windows.h>
+#include "resource.h" //표준 라이브러리가 아님 ""
+
+HINSTANCE g_hinstance;
+
+
+// DrawBitmap이라는 함수가 메모장에 적혀있는데, 
+// 이게 hdc를 복사했다가 파괴하는 과정을 반복함. 
+// 성능적으로 이슈가 없는지 검색하기
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT messageType, WPARAM wParam, LPARAM lParam)
+{
+	static BITMAP bitmapInfo;
+	PAINTSTRUCT paintStruct;
+	static HBITMAP hbitmap;
+
+	switch (messageType)
+	{
+	case WM_CREATE:
+	{
+		hbitmap = LoadBitmap(g_hinstance, MAKEINTRESOURCE(IDB_BITMAP1));
+		
+		GetObject(hbitmap,sizeof(BITMAP), &bitmapInfo); // 매개변수 중간 c는 capacity의 약자
+		
+
+		return S_OK;
+	}
+
+	case WM_PAINT:
+	{
+		HDC hdc = BeginPaint(hWnd, &paintStruct);
+
+		HDC fullScreenDC = GetDC(NULL);
+
+		BitBlt(hdc, 50, 50, 200, 200, fullScreenDC, 0, 0 ,SRCCOPY);
+		ReleaseDC(NULL, fullScreenDC);
+
+
+		//HDC memDC = CreateCompatibleDC(hdc);
+		//SelectObject(memDC, hbitmap);
+		//BitBlt(hdc, 50, 50, bitmapInfo.bmWidth, bitmapInfo.bmHeight, memDC, 0, 0, SRCCOPY);
+		//DeleteDC(memDC);
+
+		EndPaint(hWnd, &paintStruct);
+
+		return 0;
+	}
+	//case WM_KEYDOWN:
+
+	//{
+	//	HDC hdc = GetDC(hWnd);
+	//	HBITMAP hBitMap = (HBITMAP)GetCurrentObject(hdc, OBJ_BITMAP); // 핸들을 가져온다.
+
+
+	//	BITMAP bitmap;
+
+	//	GetObject(hBitMap, sizeof(BITMAP), &bitmap);
+
+
+	//	ReleaseDC(hWnd, hdc);
+	//}
+
+
+	//	return S_OK;
+
+	case WM_DESTROY:
+
+		PostQuitMessage(0);
+		DeleteObject(hbitmap);
+
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, messageType, wParam, lParam);
+}
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	g_hinstance = hInstance;
+	//WNDCLASSEX windowClass;
+	WNDCLASS windowClass;
+
+	windowClass.lpszClassName = TEXT("Second Project");
+	windowClass.lpszMenuName = NULL;
+
+	windowClass.lpfnWndProc = WndProc;
+	windowClass.hInstance = hInstance;
+
+	windowClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	windowClass.style = CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS;
+
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	RegisterClass(&windowClass);
+
+	HWND hWND = CreateWindow(windowClass.lpszClassName, windowClass.lpszClassName, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWND, nCmdShow);
+
+	MSG msg;
+
+	while (TRUE)
+	{
+		if (GetMessage(&msg, NULL, 0, 0) == FALSE)
+		{
+			break;
+		}
+
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
---
+++ second_practice/Project1/BounceBall/Main.cpp
@@ -0,0 +1,227 @@
+﻿#include <Windows.h>
+#include <time.h>
+#include "resource.h"
+
+HINSTANCE g_hinstance;
+typedef struct
+{
+	COLORREF color;
+	POINT pos;
+	POINT velocity;
+	int radius;
+} ball_t;
+
+ball_t balls[200];
+int ball_size = sizeof(balls) / sizeof(balls[0]);
+HBITMAP hbitmap;
+BITMAP bitmapInfo;
+int x;
+int y;
+
+// bitmap = static_cast<HBITMAP>(LoadImage(nullptr, filePath.c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE));
+// 경로로 이미지 불러오기 -> bmp확장자로만 바꿔주면 그냥 쓸수있게됨. 
+// win API에서는 비트맵만 지원하기때문.
+// exe에서 클릭하면 exe로부터 경로가 시작되고, 디버깅모드에서 실행하면 프로젝트에서 경로가 시작됨.
+// 즉 베포할때는 exe파일 옆에다 두고 압축해서 배포하면 된다.
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT messageType, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (messageType)
+	{
+	case WM_CREATE:
+	{
+		hbitmap = LoadBitmap(g_hinstance, MAKEINTRESOURCE(IDB_BITMAP1));
+		bitmapInfo;
+		GetObject(hbitmap, sizeof(BITMAP), &bitmapInfo);
+
+
+		srand((unsigned int)time(NULL)); // 랜덤에 cd값 넣어서 세팅?
+		RECT clientRect;
+		GetClientRect(hWnd, &clientRect);
+		
+		for(int i = 0; i < ball_size; ++i)
+		{
+			balls[i].pos.x = rand() % clientRect.right;
+			balls[i].pos.y = rand() % clientRect.bottom;
+
+			balls[i].velocity.x = (rand() % 5 + 1) * (rand() % 2 == 0 ? -1 : 1);
+			balls[i].velocity.y = (rand() % 5 + 1) * (rand() % 2 == 0 ? -1 : 1);
+
+			balls[i].radius = rand() % 5 * 5 + 10;
+
+			balls[i].color = RGB(rand() % 255, rand() % 255, rand() % 255);
+		}
+
+		SetTimer(hWnd, 1, 25, NULL);
+
+
+		return S_OK;
+	}
+
+	case WM_KEYDOWN:
+
+		switch(wParam)
+		{
+		case VK_LEFT:
+			
+			x = x - 5;
+
+			break;
+		case VK_RIGHT:
+
+			x = x + 5;
+			break;
+
+		case VK_UP:
+
+			y = y - 5;
+
+			break;
+		case VK_DOWN:
+
+			y = y + 5;
+			break;
+		}
+
+
+		return 0;
+
+	case WM_TIMER:
+	{
+		RECT clientRect;
+		GetClientRect(hWnd, &clientRect);
+
+		for(int i = 0; i < ball_size; ++i)
+		{
+			{
+				if(balls[i].pos.x <= balls[i].radius)
+				{
+					balls[i].pos.x = balls[i].radius;
+					balls[i].velocity.x *= -1;
+				}
+
+				if(balls[i].pos.x >= clientRect.right - balls[i].radius)
+				{
+					balls[i].pos.x = clientRect.right - balls[i].radius;
+					balls[i].velocity.x *= -1;
+				}
+
+				if(balls[i].pos.y <= balls[i].radius)
+				{
+					balls[i].pos.y = balls[i].radius;
+					balls[i].velocity.y *= -1;
+				}
+
+				if (balls[i].pos.y >= clientRect.bottom - balls[i].radius)
+				{
+					balls[i].pos.y = clientRect.bottom - balls[i].radius;
+					balls[i].velocity.y *= -1;
+				}
+
+			}
+
+			balls[i].pos.x += balls[i].velocity.x;
+			balls[i].pos.y += balls[i].velocity.y;
+		}
+
+		InvalidateRect(hWnd, NULL, FALSE);
+		return S_OK;
+	}
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+
+		HDC bitmapDC = CreateCompatibleDC(hdc);
+		SelectObject(bitmapDC, hbitmap);
+
+		HDC memDC = CreateCompatibleDC(hdc);
+
+		RECT clientRect;
+		GetClientRect(hWnd, &clientRect);
+
+
+
+		HBITMAP memBitmap = CreateCompatibleBitmap(hdc, clientRect.right, clientRect.bottom);
+		HBITMAP rollbackBitmap = (HBITMAP)SelectObject(memDC, memBitmap);
+
+		FillRect(memDC, &clientRect, (HBRUSH)GetStockObject(WHITE_BRUSH));
+
+		for(int i = 0; i < ball_size; ++i)
+		{
+			SelectObject(memDC, GetStockObject(DC_BRUSH));
+			SetDCBrushColor(memDC, balls[i].color);
+
+			Ellipse(memDC, balls[i].pos.x - balls[i].radius, balls[i].pos.y - balls[i].radius,
+				balls[i].pos.x + balls[i].radius, balls[i].pos.y + balls[i].radius);
+		}
+
+		BitBlt(memDC, x, y, bitmapInfo.bmWidth, bitmapInfo.bmHeight, bitmapDC, 0, 0, SRCCOPY);
+		BitBlt(hdc, 0, 0, clientRect.right, clientRect.bottom, memDC, 0, 0, SRCCOPY);
+
+		DeleteObject(SelectObject(memDC, rollbackBitmap));
+		DeleteDC(bitmapDC);
+		DeleteDC(memDC);
+
+
+		EndPaint(hWnd, &ps);
+
+
+		return 0;
+	}
+	case WM_DESTROY:
+
+		KillTimer(hWnd, 1);
+		DeleteObject(hbitmap);
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, messageType, wParam, lParam);
+}
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	g_hinstance = hInstance;
+	//WNDCLASSEX windowClass;
+	WNDCLASS windowClass;
+
+	windowClass.lpszClassName = TEXT("Second Project");
+	windowClass.lpszMenuName = NULL;
+
+	windowClass.lpfnWndProc = WndProc;
+	windowClass.hInstance = hInstance;
+
+	windowClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	windowClass.style = CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS;
+
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	RegisterClass(&windowClass);
+
+	HWND hWND = CreateWindow(windowClass.lpszClassName, windowClass.lpszClassName, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWND, nCmdShow);
+
+	MSG msg;
+
+	while (TRUE)
+	{
+		if (GetMessage(&msg, NULL, 0, 0) == FALSE)
+		{
+			break;
+		}
+
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
